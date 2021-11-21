40758b5e3c83c584a18b56903d7418e5682583d1
vlabu <vlabum@gmail.com>
2021-11-21 16:46:55 +0500

    VeeamLab1.2

---
+++ VeeamLab1/VeeamLab2/EventHandler1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VeeamLab2
+{
+    class EventHandler1
+    {
+        public string NameHandler { get; set; }
+        public EventHandler1(string nameHandler, EventBus bus)
+        {
+            NameHandler = nameHandler;
+            bus.NewEvent += HandleEvent;
+        }
+
+        private void HandleEvent(object sender, CustomEventArgs arg)
+        {
+            Console.WriteLine($"{this.GetType()} {NameHandler} {arg.Message}");
+        }
+
+        public void Unregister(EventBus bus)
+        {
+            bus.NewEvent -= HandleEvent;
+        }
+    }
+}
---
+++ VeeamLab1/VeeamLab2/EventHandler2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VeeamLab2
+{
+    class EventHandler2
+    {
+        public string NameHandler { get; set; }
+        public EventHandler2(string nameHandler, EventBus bus)
+        {
+            NameHandler = nameHandler;
+            bus.NewEvent += HandleEvent;
+        }
+
+        private void HandleEvent(object sender, CustomEventArgs arg)
+        {
+            Console.WriteLine($"{this.GetType()} {NameHandler} {arg.Message}");
+        }
+
+        public void Unregister(EventBus bus)
+        {
+            bus.NewEvent -= HandleEvent;
+        }
+    }
+}
